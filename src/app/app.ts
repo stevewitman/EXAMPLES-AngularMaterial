2164e13f0a81ba3b9702dfac76f0428a67bf7dcb
Steve Witman <stevewitman@gmail.com>
2025-12-29 11:21:24 -0700

    add tabs component example

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MatDividerModule } from '@angular/material/divider';
+
+import { Tabs } from './examples/tabs/tabs'
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatDividerModule, Tabs],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('angular-material-examples');
---
+++ src/app/examples/tabs/tabs.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+
+import { MatTabsModule } from '@angular/material/tabs';
+
+@Component({
+  selector: 'app-tabs',
+  imports: [MatTabsModule],
+  templateUrl: './tabs.html',
+  styleUrl: './tabs.scss',
+})
+export class Tabs {
+  idfs = [
+    {
+      idf: '122'
+    },
+    {
+      idf: '145'
+    },
+    {
+      idf: '168'
+    },
+    {
+      idf: '222'
+    },
+    {
+      idf: '245'
+    },
+    {
+      idf: '268'
+    },
+    {
+      idf: '322'
+    },
+    {
+      idf: '345'
+    },
+    {
+      idf: '368'
+    },
+    {
+      idf: '422'
+    },
+    {
+      idf: '445'
+    },
+    {
+      idf: '468'
+    },
+    {
+      idf: '522'
+    },
+    {
+      idf: '545'
+    },
+    {
+      idf: '568'
+    },
+    {
+      idf: '1B138'
+    },
+
+  ]
+}
